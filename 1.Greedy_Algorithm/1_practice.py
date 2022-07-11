0e947c4a64f0621fafc411bf4863d5c19e9d973b
YOON-CC <whdbscks77@gmail.com>
2022-07-11 22:33:48 +0900

    그리디

--- 1.Greedy_Algorithm/1_practice.py
+++ 1.Greedy_Algorithm/1_practice.py
@@ -5,10 +5,10 @@ box = []
 for i in range(n):
     box.append(list(map(int, input().split())))
 
-box.sort(key = lambda x: (x[1], x[0]))
+box.sort(key = lambda x: (x[1], x[0])) # 람다식을 이용하여 정렬의 우선순위를 나타냄
 
 ans = end = 0
-for s, e in box:
+for s, e in box: # s,e에는 하나의 배열에 첫번째, 두번째 값이 담긴다.
     if s >= end:
         ans += 1
         end = e

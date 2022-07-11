cc46fa0ff96f0e2c74e74542a16476f064953d6d
YOON-CC <whdbscks77@gmail.com>
2022-07-11 22:32:28 +0900

    그리디

--- 1.Greedy_Algorithm/1_practice.py
+++ 1.Greedy_Algorithm/1_practice.py
@@ -1,1 +1,15 @@
-#탐욕알고리즘
+#1931
+n = int(input())
+
+box = []
+for i in range(n):
+    box.append(list(map(int, input().split())))
+
+box.sort(key = lambda x: (x[1], x[0]))
+
+ans = end = 0
+for s, e in box:
+    if s >= end:
+        ans += 1
+        end = e
+print(ans)

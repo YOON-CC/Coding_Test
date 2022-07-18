51d7fc8bc7582e9c35aa35661775dabce821a109
YOON-CC <whdbscks77@gmail.com>
2022-07-18 21:45:22 +0900

    이진탐색(라이브러리)

--- 4.Binary_Search/1_practice.py
+++ 4.Binary_Search/1_practice.py
@@ -1,3 +1,5 @@
+##이진탐색
+
 def binary_search(array, target, start, end):
     if start > end:
         return None
@@ -12,10 +14,14 @@ def binary_search(array, target, start, end):
 
 n, target = list(map(int, input().split()))
 
-array = list(map(int, input().split()))
+array = list(map(int, input().split())) ## 정렬된 값이 들어와야 한다.
 
 result = binary_search(array, target, 0, n-1)
 if result == None:
     print("원소가 존재하지 않습니다.")
 else:
     print(result+1)
+    
+#그러나 파이썬에서는 라이브러리를 활용한다.
+#https://programming119.tistory.com/196
+#코딩 테스트에서 유용하게 사용이 된다.
